72ba2ea57cf9e619a2eee04eeb979f5e644c58e7
drozddmitriy <drozd.dmitriy.etc@gmail.com>
2018-12-07 20:12:35 +0200

    send_email

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,11 +9,44 @@ use App\Service;
 use App\Portfolio;
 use App\People;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class IndexController extends Controller
 {
     public function execute(Request $request)
     {
+        if($request->isMethod('post')){
+
+            $messages = [
+                'required' => "Поле :atribute обязательно к заполнению",
+                'email' => "Поле :atribute должно соответствовать email адресу"
+            ];
+
+            $this->validate($request,[
+                'name' => 'required|max:255',
+                'email' => 'required|email',
+                'text' => 'required'
+            ], $messages);
+
+            $data = $request->all();
+
+            //dd($data['email']);
+
+            $result = Mail::send('site.email', ['data' => $data], function ($message) use ($data){
+                $mail_admin = env('MAIL_ADMIN');
+                $message->from($data['email'], $data['name']);
+                $message->to($mail_admin)->subject('Question');
+
+            });
+
+
+            if($result){
+                return redirect()->route('home')->with('status','Email is send');
+            }
+            ///mail
+
+        }
+
         $pages = Page::all();
         $portfolios = Portfolio::get(array('name', 'filter', 'images'));
         $services = Service::where('id', '<', 20)->get();
